9888624bc1bf6babc904b7b87e4d3979d166b3dc
Fabien Dehopré <fabien@dehopre.com>
2020-07-29 11:13:11 +0200

    remove some rxjs operators in search
    the interviewee need to add them again for the following question/statement:
    Avoid calling the searchHeroes service method each time the use type something in the search field + do not call the service method more than once with the same search term

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Observable, Subject } from 'rxjs';
-
-import {
-   debounceTime, distinctUntilChanged, switchMap
- } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -26,15 +22,6 @@ export class HeroSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.heroes$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
-
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
-
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => this.heroService.searchHeroes(term)),
-    );
+    this.heroes$ = this.searchTerms.pipe(switchMap((term: string) => this.heroService.searchHeroes(term)));
   }
 }
