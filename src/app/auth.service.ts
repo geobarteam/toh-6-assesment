7fc0ff0e8b5409ab4ee3a08a597baf5a9d837746
Fabien Dehopré <fabien@dehopre.com>
2020-07-29 15:22:43 +0200

    add a form to login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
+  constructor(
+    private readonly router: Router,
+    private readonly messageService: MessageService
+  ) {}
 
-  constructor() { }
+  login(user: string, passwd: string, redirectTo?: string): void {
+    if (user === 'flx' && passwd === 'Fluxys@2020') {
+      this.log('Login success');
+      sessionStorage.setItem('toh-auth', '1');
+      if (redirectTo != undefined && redirectTo !== '') {
+        this.router.navigateByUrl(redirectTo);
+      } else {
+        this.router.navigate(['']);
+      }
+    } else {
+      this.log('Invalid username or password! Try again.');
+    }
+  }
+
+  isLoggedIn(): boolean {
+    return sessionStorage.getItem('toh-auth') === '1';
+  }
+
+  logout(): void {
+    this.log('Logout success');
+    sessionStorage.removeItem('toh-auth');
+    this.router.navigate(['']);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: { user: string; passwd: string } = { user: '', passwd: '' };
 
-  constructor() { }
+  constructor(
+    private readonly auth: AuthService,
+    private readonly activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
   }
 
+  signIn(): void {
+    const redirectTo = this.activatedRoute.snapshot.queryParamMap.get('redirectTo');
+    this.auth.login(this.form.user, this.form.passwd, redirectTo);
+  }
 }
